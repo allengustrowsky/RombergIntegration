b8f5eadeaf6bee2502765ecb742f6828b71dc0b2
Allen Gustrowsky <allen@may21.us>
2023-03-14 19:49:22 -0400

    Code cleanup, removed compiler warnings

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod config;
 use config::romberg_config;
-use std::convert::TryInto;
-use std::time::{Duration, Instant};
+use std::time::{Instant};
 
+#[allow(unused_assignments)]
 fn main() {
     let start_time = Instant::now();
     let params: romberg_config::Values = Default::default();
@@ -11,7 +11,6 @@ fn main() {
     let mut fs_coeff: f64; // coefficient for expression in first sweep
     let mut c: f64; // coefficient for second expression in sweeps 2+
     let mut sum: f64 = 0.0; // used in first sweep only
-    // c = 1.0 / (((4i64.pow(2))-1) as f64);
     // initialize Romberg table
     let mut r_table: Vec<Vec<f64>> = vec![vec![]; params.num_iterations as usize];
     
@@ -22,35 +21,22 @@ fn main() {
         let fb: f64 = (params.equation)(params.upper_bound);
         for n_coeff in 1..2i64.pow(i as u32) {
             sum += (params.equation)(params.lower_bound + ((h*(n_coeff as f64)) / (2i64.pow(i as u32)) as f64));
-            // println!("f({} + {}h/{})", params.lower_bound, n_coeff as f64, (2i64.pow(i as u32)) as f64);
         }
-        // println!("fa: {}", fa);
-        // println!("final: {}[{} + 2({}) + {}]", fs_coeff, fa, sum, fb);
         r_table[0].push(fs_coeff * (fa + 2.0*sum + fb));
         sum = 0.0;
     }
-    // println!("1st sweep: {:?}", r_table);
 
     // perform remaining sweeps
     for k in 1..params.num_iterations { // references array
-        // println!("i: {}", k);
         c = 1.0 / (((4i64.pow(k as u32))-1) as f64);
-        // println!("c: {}", c);
         for j in 1..(params.num_iterations-k+1) { // references array index
-            // println!("j: {}", j);
-            // println!("{} + {}*({} - {})", r_table[k-1][j], c, r_table[k-1][j], r_table[k-1][j-1]);
             sum = r_table[k-1][j] + c*(r_table[k-1][j] - r_table[k-1][j-1]);
-            // println!("safe!");
             r_table[k].push(sum);
-            // println!("safe2");
             sum = 0.0;
         }
-        // println!("sweep {}: {:?}", k+1, r_table);
     }
-    // println!("done: {:?}", r_table);
-    // println!("result: {}", r_table[params.num_iterations-1][0]);
 
-    // elapsed time adapted from https://rust-lang-nursery.github.io/rust-cookbook/datetime/duration.html#measure-the-elapsed-time-between-two-code-sections
+    // start_time and calc_time adapted from https://rust-lang-nursery.github.io/rust-cookbook/datetime/duration.html#measure-the-elapsed-time-between-two-code-sections
     let calc_time = start_time.elapsed();
 
     // print result
@@ -60,7 +46,6 @@ fn main() {
     // print Romberg table 
     if params.print_table {
         println!();
-        let wer: String = String::from("hello");
         for row in 0..r_table.len() {
             print!("{:^29}", format!("{} {}", String::from("sweep"), row + 1));
         }
